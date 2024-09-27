cf176ab4376495ed534b395aeb6cc913a44999a1
Arash <sainiarash555@gmail.com>
2024-09-27 12:37:00 +0530

    bookstore

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require("express");
+const app=express();
+const mongoose=require("mangoose");
+const dotenv= require("dotenv")
+dotenv.config();
+const bookroute=require("./routes/bookroute");
+mangoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>console.log("connected to mongodbb")).catch(err=>console.log(err));
+app.use(express.json());
+app.use("/api/user",bookroutr);
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>
+console.log(`server is listening on PORT ${PORT}`));
+
